a3adf664552431e056eac74fac91d54311d0f567
lrc1043 <lrc1043@rambler.ru>
2021-08-05 15:52:45 +0300

    Add files via upload

--- js/card/cards.js
+++ js/card/cards.js
@@ -88,10 +88,10 @@ var CardText = React.createClass({
       }
     } else {
       // if (len > 1150) {
-        if (len > 1150) {
+        if (len > 1100) {
           fontSizeClass += ' small';
         }
-        else if (len > 1250) {
+        else if (len > 1200) {
           fontSizeClass += ' verysmall';
         }
         else {
